71cc67f301fab4d60f3b8bd149f82e7e264b11f0
tiandonggu <gtd20151314@163.com>
2020-01-22 20:14:21 +0800

    musciapp

---
+++ dist/js/setList.js
@@ -0,0 +1,1 @@
+!function(o){(window.player||(window.player={})).setList=function(n){var i="";n.forEach(function(n){i+='<li class="songlist"><em>'+n.song+"</em>"+n.singer+"</li>"}),o(".song-list-wapper").append(i)}}(window.Zepto);
