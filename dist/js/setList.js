67cf3d27f29fe73c2915479f633b3d74f3890066
tiandonggu <gtd20151314@163.com>
2020-01-23 10:35:28 +0800

    refresh

--- dist/js/setList.js
+++
@@ -1,1 +0,0 @@
-!function(o){(window.player||(window.player={})).setList=function(n){var i="";n.forEach(function(n){i+='<li class="songlist"><em>'+n.song+"</em>"+n.singer+"</li>"}),o(".song-list-wapper").append(i)}}(window.Zepto);
